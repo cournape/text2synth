8c46cc2b2e9f43cca77c32a0597af9af9dde1af9
David Cournapeau <cournape@gmail.com>
2025-10-14 22:46:34 +0900

    TST: add basic test for MCP server.

--- tests/test_mcp_server.py
+++ tests/test_mcp_server.py
@@ -1,11 +1,16 @@
 import inspect
+import io
 import textwrap
 
 from typing import Optional
+from unittest import mock
 
+from fastmcp import Client
+from mcp.types import Tool, CallToolRequest, CallToolResult, TextContent
 from pydantic import BaseModel, Field
 
-from text2synth.mcp_server import create_function_from_model
+from text2synth.mcp_server import INIT_106, create_function_from_model
+from text2synth.state import JU06AState
 
 
 class TestToolFactory:
@@ -37,8 +42,8 @@ class TestToolFactory:
     def test_simple(self):
         # Given
         class SimpleModel(BaseModel):
-            a: int = Field(description="first arg") 
-            b: int = Field(description="second arg") 
+            a: int = Field(description="first arg")
+            b: int = Field(description="second arg")
 
         def func_ref(a: Optional[int] = None, b: Optional[int] = None) -> None:
             pass
@@ -65,8 +70,8 @@ class TestToolFactory:
     def test_ranges(self):
         # Given
         class SimpleModel(BaseModel):
-            a: int = Field(ge=0, le=255, description="first arg") 
-            b: int = Field(ge=0, le=127, description="second arg") 
+            a: int = Field(ge=0, le=255, description="first arg")
+            b: int = Field(ge=0, le=127, description="second arg")
 
         def func_ref(a: Optional[int] = None, b: Optional[int] = None) -> None:
             pass
@@ -91,3 +96,33 @@ class TestToolFactory:
         # Then
         assert func.__doc__ == docstring_ref
         assert func.__annotations__ == func_ref.__annotations__
+
+
+class TestText2SynthMCPIntegration:
+    @mock.patch('text2synth.mcp_server.apply_state_to_synth')
+    async def test_reset(self, apply_state_to_synth):
+        # Given
+        from text2synth.mcp_server import server
+
+        # When
+        async with Client(server) as client:
+            result = await client.call_tool("reset", {})
+
+        # Then
+        assert apply_state_to_synth.called
+        assert not result.is_error
+
+    @mock.patch('text2synth.mcp_server.apply_state_to_synth')
+    async def test_update_synth_state(self, apply_state_to_synth):
+        # Given
+        from text2synth.mcp_server import server
+
+        r_state = JU06AState.from_file(io.StringIO(INIT_106))
+        r_state.cutoff = 42
+
+        # When
+        async with Client(server) as client:
+            result = await client.call_tool("update_synth_state", {"cutoff": r_state.cutoff})
+
+        # Then
+        apply_state_to_synth.assert_called_with(r_state)
